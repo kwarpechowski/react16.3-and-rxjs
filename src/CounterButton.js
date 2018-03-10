9fcb74bee6cced0db9e370a11cf5909df184c553
Kamil Warpechowski <k.warpechowski@hakama.pl>
2018-03-10 23:35:05 +0100

    add babel config

--- src/CounterButton.js
+++ src/CounterButton.js
@@ -3,8 +3,10 @@ import CounterContext from './CounterContext';
 
 export const CounterButton = () => (
     <CounterContext.Consumer>
-        {({isActive, actions: { toggleAction }}) => {
-            return  <button onClick={toggleAction}>{isActive ? 'cancel': 'start'}</button>
-        }}
+        {({isActive, actions: { toggleAction }}) => (
+            <button onClick={toggleAction}>
+                {isActive ? 'cancel': 'start'}
+            </button>
+        )}
     </CounterContext.Consumer>
 );
